15f31aeacadcdfb99e2864972f71941051bac7a1
Angus109 <tobechinlems01@gmail.com>
2024-10-19 02:43:54 -0700

    updated

--- src/routes/speakers.route.ts
+++ src/routes/speakers.route.ts
@@ -2,9 +2,9 @@ import express from 'express'
 const router = express.Router()
 import auth from '../middlewares/auth'
 import { createSpeakers, updateSpeakers, deleteSpeaker, getSpeakersAll} from '../controllers/speakers.controller'
-import { createSponsor } from '../controllers/sponsors.controller'
 
- router.post('/', auth, createSponsor )
+
+ router.post('/', auth, createSpeakers)
  router.put('/action', auth, updateSpeakers)
  router.delete('/action', auth, deleteSpeaker)
  router.get('/', auth, getSpeakersAll)
