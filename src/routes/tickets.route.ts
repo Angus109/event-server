c963c448b977b5218028c7c3436d7cf4c0061db5
Angus109 <tobechinlems01@gmail.com>
2024-10-19 02:43:01 -0700

    updated

--- src/routes/tickets.route.ts
+++ src/routes/tickets.route.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 const router = express.Router()
 import auth from '../middlewares/auth'
-import { createSpeakers, deleteSpeaker, getSpeakersAll, updateSpeakers } from '../controllers/speakers.controller'
 
- router.post('/', auth, createSpeakers)
- router.put('/action', auth, updateSpeakers)
- router.delete('/action', auth, deleteSpeaker)
- router.get('/', auth, getSpeakersAll)
+import { createTicket, updateTicket, deleteTicket,getTicketsAll } from '../controllers/tickets.controller'
+ router.post('/',  createTicket)
+ router.put('/action', auth, updateTicket)
+ router.delete('/action', auth, deleteTicket)
+ router.get('/', auth, getTicketsAll)
 
  
 
