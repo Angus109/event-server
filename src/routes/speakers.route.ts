e9ab26be4bfdf528c7375ba3ef855a38ad3810c9
unknown <tobechinlems01@gmail.com>
2024-11-29 10:29:47 +0100

    updated

--- src/routes/speakers.route.ts
+++ src/routes/speakers.route.ts
@@ -4,7 +4,7 @@ import auth from '../middlewares/auth'
 import { createSpeakers, updateSpeakers, deleteSpeaker, getSpeakersAll} from '../controllers/speakers.controller'
 
 
- router.post('/', auth, createSpeakers)
+ router.post('/', createSpeakers)
  router.put('/action', auth, updateSpeakers)
  router.delete('/action', auth, deleteSpeaker)
  router.get('/', auth, getSpeakersAll)
